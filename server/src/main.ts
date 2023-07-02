4b58da25edaade58b072f26e54a220baebb5359b
Albert Akrong <alphaxsalt@gmail.com>
2023-07-02 11:39:36 +0100

    fix: remove openapi init

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,18 +1,19 @@
 import 'dotenv-defaults/config';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import config from './infra/config/app.config';
 import validationConfig from './infra/config/validation.config';
 import { RootModule } from './root.module';
-import { initializeOpenApi } from './tools/openapi';
 
 export default async function startApplication() {
-  const app = await NestFactory.create(RootModule);
-  initializeOpenApi(app);
+  const app = await NestFactory.create<NestExpressApplication>(RootModule);
+  // initializeOpenApi(app);
   app.enableCors({
     origin: '*',
   });
   app.useGlobalPipes(new ValidationPipe(validationConfig));
+  app.disable('x-powered-by');
   await app.listen(config.server.port);
 }
 
