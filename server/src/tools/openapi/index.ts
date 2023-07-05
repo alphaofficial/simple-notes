09ab4801e62c8821d93947d469f29106ee169e16
Albert Akrong <alphaxsalt@gmail.com>
2023-07-05 20:27:28 +0100

    feat: use swagger api json

--- server/src/tools/openapi/index.ts
+++ server/src/tools/openapi/index.ts
@@ -1,25 +1,7 @@
 import { INestApplication } from '@nestjs/common';
-import {
-  DocumentBuilder,
-  SwaggerDocumentOptions,
-  SwaggerModule,
-} from '@nestjs/swagger';
-import * as fs from 'fs';
-import * as path from 'path';
+import { SwaggerModule } from '@nestjs/swagger';
+import apiSpec from './api.json';
 
 export function initializeOpenApi(app: INestApplication) {
-  const config = new DocumentBuilder()
-    .setTitle('NotesApp API')
-    .setDescription('The NotesApp API description')
-    .setVersion('1.0')
-    .build();
-
-  const options: SwaggerDocumentOptions = {
-    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
-  };
-  const document = SwaggerModule.createDocument(app, config, options);
-  const dir = path.join(process.cwd(), 'src', 'shared', 'openapi', 'api.json');
-  fs.writeFileSync(dir, JSON.stringify(document));
-
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api', app, apiSpec as any);
 }
